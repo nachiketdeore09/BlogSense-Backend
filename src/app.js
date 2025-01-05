cb09776ad562c878c38fa3b454847531be170892
Nachiket <nachiketdeore10@gmail.com>
2025-01-05 20:16:25 +0530

    initialize project with Express, MongoDB, and essential configurations

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+}))
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+export default app;
