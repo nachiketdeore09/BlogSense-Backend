b092f27f17c9079df4e0a5c699a76fb19ac3cacc
Nachiket <nachiketdeore10@gmail.com>
2025-02-12 22:46:36 +0530

    Ollama commented

--- src/utils/generateAnswer.js
+++ src/utils/generateAnswer.js
@@ -1,25 +1,25 @@
-import ollama from 'ollama';
+// import ollama from 'ollama';
 
-const generateAnswer = async (context, question) => {
-    const response = await ollama.chat({
-        model: 'mistral', // or "llama2"
-        messages: [
-            {
-                role: 'system',
-                content:
-                    "You are a helpful assistant. Answer questions strictly based on the provided context.If the context does not contain relevant information, say 'I don't know based on the provided context.' Do not generate or assume any information beyond the context.",
-            },
-            {
-                role: 'user',
-                content: `Context: ${context}\n\nQuestion: ${question}`,
-            },
-        ],
-    });
+// const generateAnswer = async (context, question) => {
+//     const response = await ollama.chat({
+//         model: 'mistral', // or "llama2"
+//         messages: [
+//             {
+//                 role: 'system',
+//                 content:
+//                     "You are a helpful assistant. Answer questions strictly based on the provided context.If the context does not contain relevant information, say 'I don't know based on the provided context.' Do not generate or assume any information beyond the context.",
+//             },
+//             {
+//                 role: 'user',
+//                 content: `Context: ${context}\n\nQuestion: ${question}`,
+//             },
+//         ],
+//     });
 
-    console.log(response)
+//     console.log(response)
 
-    return response.message.content;
-};
+//     return response.message.content;
+// };
 
 
-export default generateAnswer;
+// export default generateAnswer;
